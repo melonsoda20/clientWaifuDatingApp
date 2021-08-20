6b4bf1ddf182b2fb76dae6f8108efe126084263d
melonsoda20 <benedictusteddy200596@gmail.com>
2021-08-20 18:38:52 +0700

    Add not found component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
@@ -20,6 +21,7 @@ const routes: Routes = [
       { path: 'messages', component: MessagesComponent }
     ]},
   {path: 'errors', component: TestErrorsComponent },
+  { path: 'not-found', component: NotFoundComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' }
 ];
 
