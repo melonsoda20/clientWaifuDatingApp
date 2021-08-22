65fa902ea7dff9675df11bd6529be040fa29f68b
melonsoda20 <benedictusteddy200596@gmail.com>
2021-08-22 20:28:44 +0700

    Update members service to function as a client store for members data

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/models/member.model';
 import { MembersService } from 'src/services/members.service';
 
@@ -8,18 +9,11 @@ import { MembersService } from 'src/services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members: Member[];
+  members$: Observable<Member[]>;
 
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
+    this.members$ = this.memberService.getMembers();
   }
-
-  loadMembers() {
-    this.memberService.getMembers().subscribe(members => {
-      this.members = members;
-    });
-  }
-
 }
