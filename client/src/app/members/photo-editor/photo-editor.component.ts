6faf7c8dac848bdffc07bf4bf526ad360be2ec27
melonsoda20 <benedictusteddy200596@gmail.com>
2021-08-23 22:35:54 +0700

    Create feature to add photo in the edit page

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Login } from 'src/models/login.model';
 import { Member } from 'src/models/member.model';
+import { AccountService } from 'src/services/account.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -8,10 +13,42 @@ import { Member } from 'src/models/member.model';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() member: Member; 
+  uploader: FileUploader;
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: Login
 
-  constructor() { }
+  constructor(private accountService: AccountService) {
+    this.accountService.getCurrentUser().pipe(take(1)).subscribe(user => this.user = user)
+   }
 
   ngOnInit(): void {
+    this.initializeUploader();
   }
+
+  fileOverBase(e: any){
+    this.hasBaseDropzoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      const photo = JSON.parse(response);
+      this.member.photos.push(photo);
+    }
+  } 
 
 }
