c92c34c708bcb730ca3901822e7e6e596d2e7db6
melonsoda20 <benedictusteddy200596@gmail.com>
2021-10-28 14:12:53 +0700

    Add caching for members page

--- client/src/services/members.service.ts
+++ client/src/services/members.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from 'src/models/member.model';
@@ -13,10 +13,15 @@ import { UserParams } from 'src/models/userParams';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
 
   constructor(private http: HttpClient) { }
 
-  getMembers(userParams: UserParams): Observable<PaginatedResult<Member[]>>{    
+  getMembers(userParams: UserParams): Observable<PaginatedResult<Member[]>>{
+    var response = this.memberCache.get(Object.values(userParams).join('-'));
+    if(response){
+      return of(response);
+    }    
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
     params = params.append('minAge', userParams.minAge.toString());
@@ -24,7 +29,11 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
     
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
+      .pipe(map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      }));
   }
 
   private getPaginatedResult<T>(url, params): Observable<PaginatedResult<T>> {
